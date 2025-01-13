7c9fc6bce5126da0bfde78cc5e4fa82c9a476491
Jonathan Sanfilippo <jonjon5997@gmail.com>
2025-01-13 12:02:30 -0500

    update controller controllers/users.js to handle errors

---
+++ controllers/users.js
@@ -0,0 +1,17 @@
+const User = require("../models/user");
+
+//GET users
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => {
+      throw Error("AHH!!");
+      res.send(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { getUsers };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const { getUsers } = require("../controllers/users");
 
-router.get("/", () => console.log("GET users"));
+router.get("/", getUsers);
 router.get("/:userId", () => console.log("GET users by ID"));
 router.post("/", () => console.log("POST users"));
 
