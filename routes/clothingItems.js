a0ca295eb15118bfbfb42db5f67bb78a59cba635
Jonathan Sanfilippo <jonjon5997@gmail.com>
2025-01-29 11:06:53 -0500

    import authentication middleware

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const auth = require("../middlewares/auth"); // Import authentication middleware
 const {
   createItem,
   getItems,
@@ -8,13 +8,11 @@ const {
   unlikeItem,
 } = require("../controllers/clothingItems");
 
-router.post("/", createItem);
+router.get("/", getItems); // No auth required
 
-router.get("/", getItems);
-
-router.delete("/:itemId", deleteItem);
-
-router.put("/:itemId/likes", likeItem);
-router.delete("/:itemId/likes", unlikeItem);
+router.post("/", auth, createItem);
+router.delete("/:itemId", auth, deleteItem);
+router.put("/:itemId/likes", auth, likeItem);
+router.delete("/:itemId/likes", auth, unlikeItem);
 
 module.exports = router;
