a5490aac1051397a9b35e10e4ec6ce28de89cecd
Jonathan Sanfilippo <jonjon5997@gmail.com>
2025-03-13 18:43:30 -0400

    refactor auth file to use error class and update weather values to checked values

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,13 @@
 // Imports
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { ERROR_CODES } = require("../utils/constants");
+const UnauthorizedError = require("../errors/unauthorizedError"); // Import error class
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    res
-      .status(ERROR_CODES.UNAUTHORIZED)
-      .send({ message: "Authorization required" });
-    return; // Explicitly return to ensure no further execution
+    return next(new UnauthorizedError("Authorization required"));
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -20,9 +17,7 @@ const auth = (req, res, next) => {
     req.user = payload; // Attach the token payload to the request object
     next(); // Proceed to the next middleware or route handler
   } catch (err) {
-    res
-      .status(ERROR_CODES.UNAUTHORIZED)
-      .send({ message: "Invalid or expired token" });
+    next(new UnauthorizedError("Invalid or expired token"));
   }
 };
 
